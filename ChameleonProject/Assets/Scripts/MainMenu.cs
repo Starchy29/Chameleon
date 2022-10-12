282a21facd9747a489c6d743c80ad688e883591d
ZaffyPuck <56975883+ZaffyPuck@users.noreply.github.com>
2022-10-12 00:33:56 -0400

    Created Menu Scene
    Works

---
+++ ChameleonProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads desired scene
+    /// </summary>
+    /// <param name="scene">The scenes number</param>
+    public void LoadScene(int scene)
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + scene); // This gets the next scene in the build index - not necessary at the moment
+        SceneManager.LoadScene(scene);
+    }
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
